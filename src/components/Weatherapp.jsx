cf5e25f4ed9dc64981a7b6686b5cda8df5e37c62
leanwithkumar <mrhustle936@gmail.com>
2025-06-14 16:27:31 +0530

    all final done

--- src/components/Weatherapp.jsx
+++ src/components/Weatherapp.jsx
@@ -13,7 +13,7 @@ function Weatherapp() {
     if (!city.trim()) return;
 
     try {
-      const response = await fetch(`https://weather-backend-cyan.vercel.app/weather?city=${encodeURIComponent(cityname)}`);
+      const response = await fetch(`https://weather-backend-bl8q.onrender.com/?city=${encodeURIComponent(cityname)}`);
       const data = await response.json();
       const temp = data.current.temp_c;
       const iconlink = data.current.condition.icon;
